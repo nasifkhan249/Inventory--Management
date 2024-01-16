03fa62090d1d64fb93757e3d888664d775e4359a
Nasif Mustufa Khan <97841869+nasifkhan249@users.noreply.github.com>
2024-01-16 21:12:33 +0600

    Updated

---
+++ src/models/Users/OTPSModel.js
@@ -0,0 +1,10 @@
+const  mongoose=require('mongoose');
+const DataSchema=mongoose.Schema({
+    email:{type:String},
+    otp:{type:String},
+    status:{type:Number,default:0},
+    createdDate:{type:Date,default:Date.now()}
+},{versionKey:false});
+const OTPsModel=mongoose.model('otps',DataSchema);
+module.exports=OTPsModel;
+
